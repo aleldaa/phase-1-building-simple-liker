9ecc8d0083685eb6bca6085ee2cdbbb2315c3871
Luna Daniels <aleldaproductions@gmail.com>
2023-03-27 11:53:46 -0600

    finished lab

--- main.js
+++ main.js
@@ -3,9 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.querySelectorAll('.like-glyph')
 
 
 
+function heartReact(event){
+  const heart = event.target
+  console.log(heart)
+  mimicServerCall()
+  .then(() => {
+    //if statement to check if heart is full, if heart is already full make it empty, if it is empty make it full 
+    if ( heart.innerText === EMPTY_HEART) {
+      heart.innerText = FULL_HEART;
+      heart.className = "activated-heart";
+    } else {
+      heart.innerText = EMPTY_HEART;
+      heart.className = "";
+    }
+  })
+
+
+  .catch(function (error) {
+    const errorDiv = document.querySelector("#modal")
+    errorDiv.className = ""
+    errorDiv.innerText = error
+    setTimeout(() => errorDiv.className = "hidden", 3000)
+})
+}
+
+for (const heart of hearts){
+  heart.addEventListener("click", heartReact)
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
